5092fcb4324cb85070827cb5c56030103037072f
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-25 17:49:23 -0300

    12 Implementing State
    12 Implementing State

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,38 @@ import {Form,  FormControl,Button} from 'react-bootstrap';
 import './App.css';
 
 class App extends Component{ 
+
+    constructor() {
+        super ();
+        this.state = {
+            newDate: '',
+            bithday: '1900-01-01'
+        }
+    }
+
+    changeBirthday() {
+        console.log(this.state);
+        this.setState({bithday:this.state.newDate});
+    }
+
+
     render() {
         return (
             <div className="App">
                 <Form inline>
                 <h2>Input your Birthday !</h2>
-                    <FormControl type="date">
+                    <FormControl 
+                        type="date"
+                        onChange = { 
+                            event => this.setState({
+                            newDate: event.target.value })}
+                    >
 
                     </FormControl>
                     {' '}
-                    <Button> Submit </Button>
+                    <Button onClick ={
+                        () => this.changeBirthday() }
+                    > Submit </Button>
                 </Form>              
             </div>
         );
