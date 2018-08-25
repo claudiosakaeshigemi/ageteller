22dd2b71e6e17c98777d1fd64d1346d231990afb
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-25 16:45:41 -0300

    10. Creating an App Component
    10. Creating an App Component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
 ReactDOM.render(
-    <div>ageteller </div>,
-    document.getElementById('root')
+    <App / >,    document.getElementById('root')
 )
